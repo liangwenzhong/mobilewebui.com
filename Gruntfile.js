10f54cfba87b1f54b367822405b4f356c8487095
juno leung <lyuno@vip.qq.com>
2014-03-21 21:51:40 +0800

    update
    update

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+
+
+
+		connect: {
+			server: {
+				options: {
+					port: 4000,
+					base: '_site'
+				}
+			}
+		}
+
+
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-connect');
+
+	grunt.registerTask('default', ['connect']);
+
+};
